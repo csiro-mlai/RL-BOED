9b5c1463a168fab46ebce7028d2f5d73bd3feb83
Amir Dezfouli <akdezfuli@gmail.com>
2021-09-16 17:44:13 +1000

    add ql model and ql agent

---
+++ Experiments/adaptive_ql_sac.py
@@ -0,0 +1,186 @@
+"""
+Use SAC to learn an agent that adaptively designs source location experiments
+"""
+import argparse
+
+import joblib
+import torch
+import numpy as np
+
+from garage.experiment import deterministic
+from garage.torch import set_gpu_mode
+from pyro import wrap_experiment, set_rng_seed
+from pyro.algos import SAC
+from pyro.envs import AdaptiveDesignEnv, GymEnv, normalize
+from pyro.envs.adaptive_design_env import LOWER, UPPER, TERMINAL
+from pyro.experiment import Trainer
+from pyro.models.adaptive_experiment_model import SourceModel
+from pyro.models.ql_model import QLModel
+from pyro.policies.adaptive_tanh_gaussian_policy import \
+    AdaptiveTanhGaussianPolicy
+from pyro.q_functions.adaptive_mlp_q_function import AdaptiveMLPQFunction
+from pyro.replay_buffer import PathBuffer
+from pyro.sampler.local_sampler import LocalSampler
+from pyro.sampler.vector_worker import VectorWorker
+from pyro.spaces.batch_box import BatchBox
+from torch import nn
+from dowel import logger
+
+seeds = [373693, 943929, 675273, 79387, 508137, 557390, 756177, 155183, 262598,
+         572185]
+
+
+def main(n_parallel=1, budget=1, n_rl_itr=1, n_cont_samples=10, seed=0,
+         log_dir=None, snapshot_mode='gap', snapshot_gap=500, bound_type=LOWER,
+         src_filepath=None, discount=1., alpha=None, k=2, d=2, log_info=None,
+         tau=5e-3, pi_lr=3e-4, qf_lr=3e-4, buffer_capacity=int(1e6)):
+    if log_info is None:
+        log_info = []
+    @wrap_experiment(log_dir=log_dir, snapshot_mode=snapshot_mode,
+                     snapshot_gap=snapshot_gap)
+    def sac_ql(ctxt=None, n_parallel=1, budget=1, n_rl_itr=1,
+                   n_cont_samples=10, seed=0, src_filepath=None, discount=1.,
+                   alpha=None, tau=5e-3, pi_lr=3e-4, qf_lr=3e-4,
+                   buffer_capacity=int(1e6)):
+        if log_info:
+            logger.log(str(log_info))
+
+        if torch.cuda.is_available():
+            set_gpu_mode(True)
+            torch.set_default_tensor_type('torch.cuda.FloatTensor')
+            logger.log("GPU available")
+        else:
+            set_gpu_mode(False)
+            logger.log("no GPU detected")
+        deterministic.set_seed(seed)
+        set_rng_seed(seed)
+        if src_filepath:
+            logger.log(f"loading data from {src_filepath}")
+            data = joblib.load(src_filepath)
+            env = data['env']
+            sac = data['algo']
+            if alpha is not None:
+                sac._use_automatic_entropy_tuning = False
+                sac._fixed_alpha = alpha
+        else:
+            logger.log("creating new policy")
+            layer_size = 128
+            T = 100
+            design_space = BatchBox(low=torch.as_tensor([[[[0.0, 1.0]]]]), high=torch.as_tensor([[[[0.0, 1.0]]]]))
+            obs_space = BatchBox(low=torch.as_tensor([0.0, 0.0] + [0.0] * (T * 2)),
+                                 high=torch.as_tensor([1.0, 1.0] + [1.0] * (T * 2)))
+            model = QLModel(n_parallel=n_parallel, T=T)
+
+            def make_env(design_space, obs_space, model, budget, n_cont_samples,
+                         bound_type, true_model=None):
+                env = GymEnv(
+                    normalize(
+                        AdaptiveDesignEnv(
+                            design_space, obs_space, model, budget,
+                            n_cont_samples, true_model=true_model,
+                            bound_type=bound_type),
+                        normalize_obs=True
+                    )
+                )
+                return env
+
+            def make_policy():
+                return AdaptiveTanhGaussianPolicy(
+                    env_spec=env.spec,
+                    encoder_sizes=[layer_size, layer_size],
+                    encoder_nonlinearity=nn.ReLU,
+                    encoder_output_nonlinearity=None,
+                    emitter_sizes=[layer_size, layer_size],
+                    emitter_nonlinearity=nn.ReLU,
+                    emitter_output_nonlinearity=None,
+                    encoding_dim=layer_size//2,
+                    init_std=np.sqrt(1 / 3),
+                    min_std=np.exp(-20.),
+                    max_std=np.exp(0.),
+                )
+
+            def make_q_func():
+                return AdaptiveMLPQFunction(
+                    env_spec=env.spec,
+                    encoder_sizes=[layer_size, layer_size],
+                    encoder_nonlinearity=nn.ReLU,
+                    encoder_output_nonlinearity=None,
+                    emitter_sizes=[layer_size, layer_size],
+                    emitter_nonlinearity=nn.ReLU,
+                    emitter_output_nonlinearity=None,
+                    encoding_dim=layer_size//2
+                )
+
+            env = make_env(design_space, obs_space, model, budget,
+                           n_cont_samples, bound_type)
+            policy = make_policy()
+            qf1 = make_q_func()
+            qf2 = make_q_func()
+            replay_buffer = PathBuffer(capacity_in_transitions=buffer_capacity)
+            sampler = LocalSampler(agents=policy, envs=env,
+                                   max_episode_length=budget,
+                                   worker_class=VectorWorker)
+
+            sac = SAC(env_spec=env.spec,
+                      policy=policy,
+                      qf1=qf1,
+                      qf2=qf2,
+                      replay_buffer=replay_buffer,
+                      sampler=sampler,
+                      max_episode_length_eval=budget,
+                      gradient_steps_per_itr=64,
+                      min_buffer_size=int(1e5),
+                      target_update_tau=tau,
+                      policy_lr=pi_lr,
+                      qf_lr=qf_lr,
+                      discount=discount,
+                      fixed_alpha=alpha,
+                      buffer_batch_size=4096,
+                      reward_scale=1.,)
+
+        sac.to()
+        trainer = Trainer(snapshot_config=ctxt)
+        trainer.setup(algo=sac, env=env)
+        trainer.train(n_epochs=n_rl_itr, batch_size=n_parallel * budget)
+
+    sac_ql(n_parallel=n_parallel, budget=budget, n_rl_itr=n_rl_itr,
+               n_cont_samples=n_cont_samples, seed=seed,
+               src_filepath=src_filepath, discount=discount, alpha=alpha, tau=tau, pi_lr=pi_lr, qf_lr=qf_lr,
+               buffer_capacity=buffer_capacity)
+
+    logger.dump_all()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--id", default="1", type=int)
+    parser.add_argument("--n-parallel", default="100", type=int)
+    parser.add_argument("--budget", default="30", type=int)
+    parser.add_argument("--n-rl-itr", default="50", type=int)
+    parser.add_argument("--n-contr-samples", default="10", type=int)
+    parser.add_argument("--log-dir", default=None, type=str)
+    parser.add_argument("--src-filepath", default=None, type=str)
+    parser.add_argument("--snapshot-mode", default="gap", type=str)
+    parser.add_argument("--snapshot-gap", default=500, type=int)
+    parser.add_argument("--bound-type", default="terminal", type=str.lower,
+                        choices=["lower", "upper", "terminal"])
+    parser.add_argument("--discount", default="1", type=float)
+    parser.add_argument("--alpha", default="-1", type=float)
+    parser.add_argument("--tau", default="5e-3", type=float)
+    parser.add_argument("--pi-lr", default="3e-4", type=float)
+    parser.add_argument("--qf-lr", default="3e-4", type=float)
+    parser.add_argument("--buffer-capacity", default="1e6", type=float)
+    args = parser.parse_args()
+    bound_type_dict = {"lower": LOWER, "upper": UPPER, "terminal": TERMINAL}
+    bound_type = bound_type_dict[args.bound_type]
+    exp_id = args.id
+    alpha = args.alpha if args.alpha >= 0 else None
+    buff_cap = int(args.buffer_capacity)
+    log_info = f"input params: {vars(args)}"
+    main(n_parallel=args.n_parallel, budget=args.budget, n_rl_itr=args.n_rl_itr,
+         n_cont_samples=args.n_contr_samples, seed=seeds[exp_id - 1],
+         log_dir=args.log_dir, snapshot_mode=args.snapshot_mode,
+         snapshot_gap=args.snapshot_gap, bound_type=bound_type,
+         src_filepath=args.src_filepath, discount=args.discount, alpha=alpha,
+         log_info=log_info, tau=args.tau, pi_lr=args.pi_lr,
+         qf_lr=args.qf_lr, buffer_capacity=buff_cap)
--- pyro/models/ql_model.py
+++ pyro/models/ql_model.py
@@ -2,7 +2,7 @@ from contextlib import ExitStack
 
 import numpy as np
 from torch.nn.functional import one_hot
-
+from pyro import poutine
 from pyro.contrib.util import iter_plates_to_shape, lexpand, rexpand, rmv
 from pyro.models.adaptive_experiment_model import ExperimentModel
 from pyro.util import is_bad
@@ -13,12 +13,15 @@ import pyro.distributions as dist
 EPS = 2**-22
 
 class QLModel(ExperimentModel):
-    def __init__(self, theta_mu=None, n_parallel=1, obs_label="y"):
+    def __init__(self, n_parallel=1, T = 100, obs_label="y"):
         super().__init__()
         self.n_parallel = n_parallel
         self.var_names = ["lr", "gamma"]
-        self.var_dim = 1
+        self.var_dim = 2
+        self.T = T
+        self.obs_label = obs_label
         self.sanity_check()
+
 
     def make_model(self):
         def model(design):
@@ -29,9 +32,8 @@ class QLModel(ExperimentModel):
             with ExitStack() as stack:
                 for plate in iter_plates_to_shape(batch_shape):
                     stack.enter_context(plate)
-                T = 100
-                batch_shape = design.shape[:-1]
-                theta_shape = batch_shape + (1,)
+                batch_shape = design.shape[:-2]
+                theta_shape = batch_shape + (1, 1,)
                 theta_lr = pyro.sample(
                     "lr",
                     dist.Uniform(
@@ -49,59 +51,72 @@ class QLModel(ExperimentModel):
                 )
 
                 Q = torch.rand(design.shape)
-                y = torch.zeros(batch_shape + (T, 2))
-                for i in range(T):
-                    action = pyro.sample("gamma", dist.Categorical(logits=Q * theta_gamma).to_event(2))
-                    p_reward0 = torch.gather(design, dim=2, index=action[:, :, np.newaxis])
-                    Q_a = torch.squeeze(torch.gather(Q, dim=2, index=action[:, :, np.newaxis]))
-                    p_reward = torch.cat([p_reward0, 1-p_reward0], dim=2)
-                    reward = pyro.sample("reward", dist.Categorical(probs=p_reward).to_event(2))
-                    Q = Q + theta_lr * one_hot(action, num_classes=Q.shape[2]) * (reward - Q_a)[:, :, np.newaxis]
-                    y[:, :, i, 0] = action
-                    y[:, :, i, 1] = reward
-                return y
+                y = torch.zeros(batch_shape + (1, self.T, 2))
+                for i in range(self.T):
+                    action = pyro.sample(f"a_{i}", dist.Categorical(logits=Q * theta_gamma).to_event(1))
+                    p_reward0 = torch.gather(design, dim=-1, index=action[..., np.newaxis])
+                    Q_a = torch.gather(Q, dim=-1, index=action[..., np.newaxis])
+                    p_reward = torch.cat([p_reward0, 1-p_reward0], dim=-1)
+                    reward = pyro.sample(f"r_{i}", dist.Categorical(probs=p_reward).to_event(1))
+                    Q = Q + theta_lr * one_hot(action, num_classes=Q.shape[-1]) * (reward[..., np.newaxis] - Q_a)
+                    y[..., i, 0] = action
+                    y[..., i, 1] = reward
+                return torch.squeeze(torch.flatten(y, start_dim=-2, end_dim=-1), dim=-2)
 
         return model
+
+    def get_likelihoods(self, y, design, thetas):
+        size = thetas[self.var_names[0]].shape[0]
+        cond_dict = dict(thetas)
+        cond_dict.update({self.obs_label: lexpand(y, size)})
+        cond_model = pyro.condition(self.make_model(), data=cond_dict)
+        trace = poutine.trace(cond_model).get_trace(lexpand(design, size))
+        trace.compute_log_prob()
+        likelihoods = trace.nodes[f"a_{0}"]["log_prob"] + trace.nodes[f"r_{1}"]["log_prob"]
+        for t in range(1, self.T):
+            likelihoods += trace.nodes[f"a_{t}"]["log_prob"] + trace.nodes[f"r_{t}"]["log_prob"]
+        return likelihoods
+
 
     def reset(self, n_parallel):
         self.n_parallel = n_parallel
 
 
 if __name__ == "__main__":
-
-    T = 100
-    # A x B x 1 x 1 x Design params (reward probs)
-    design = torch.rand([7, 5, 3])
-
-    batch_shape = design.shape[:-1]
-    theta_shape = batch_shape + (1,)
-    theta_lr = pyro.sample(
-        "lr",
-        dist.Uniform(
-            torch.tensor([0.0]).expand(theta_shape),
-            torch.tensor([1.0]).expand(theta_shape)
-        ).to_event(2)
-    )
-
-    theta_gamma = pyro.sample(
-        "gamma",
-        dist.Uniform(
-            torch.tensor([0.0]).expand(theta_shape),
-            torch.tensor([10.0]).expand(theta_shape)
-        ).to_event(2)
-    )
-
-    Q = torch.rand(design.shape)
-    y = torch.zeros(batch_shape + (T, 2))
-    for i in range(T):
-        action = pyro.sample("gamma", dist.Categorical(logits=Q * theta_gamma).to_event(2))
-        p_reward0 = torch.gather(design, dim=2, index=action[:, :, np.newaxis])
-        Q_a = torch.squeeze(torch.gather(Q, dim=2, index=action[:, :, np.newaxis]))
-        p_reward = torch.cat([p_reward0, 1-p_reward0], dim=2)
-        reward = pyro.sample("reward", dist.Categorical(probs=p_reward).to_event(2))
-        Q = Q + theta_lr * one_hot(action, num_classes=Q.shape[2]) * (reward - Q_a)[:, :, np.newaxis]
-        y[:, :, i, 0] = action
-        y[:, :, i, 1] = reward
-
-
-    print(y)
+    pass
+    # T = 100
+    # # A x B x 1 x 1 x Design params (reward probs)
+    # design = torch.rand([7, 5, 3])
+    #
+    # batch_shape = design.shape[:-1]
+    # theta_shape = batch_shape + (1,)
+    # theta_lr = pyro.sample(
+    #     "lr",
+    #     dist.Uniform(
+    #         torch.tensor([0.0]).expand(theta_shape),
+    #         torch.tensor([1.0]).expand(theta_shape)
+    #     ).to_event(2)
+    # )
+    #
+    # theta_gamma = pyro.sample(
+    #     "gamma",
+    #     dist.Uniform(
+    #         torch.tensor([0.0]).expand(theta_shape),
+    #         torch.tensor([10.0]).expand(theta_shape)
+    #     ).to_event(2)
+    # )
+    #
+    # Q = torch.rand(design.shape)
+    # y = torch.zeros(batch_shape + (T, 2))
+    # for i in range(T):
+    #     action = pyro.sample("gamma", dist.Categorical(logits=Q * theta_gamma).to_event(2))
+    #     p_reward0 = torch.gather(design, dim=2, index=action[:, :, np.newaxis])
+    #     Q_a = torch.squeeze(torch.gather(Q, dim=2, index=action[:, :, np.newaxis]))
+    #     p_reward = torch.cat([p_reward0, 1-p_reward0], dim=2)
+    #     reward = pyro.sample("reward", dist.Categorical(probs=p_reward).to_event(2))
+    #     Q = Q + theta_lr * one_hot(action, num_classes=Q.shape[2]) * (reward - Q_a)[:, :, np.newaxis]
+    #     y[:, :, i, 0] = action
+    #     y[:, :, i, 1] = reward
+    #
+    #
+    # print(y)
