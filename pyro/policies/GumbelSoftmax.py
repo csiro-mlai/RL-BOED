fix straight through gumbel softmax estimator

pyro/policies/GumbelSoftmax.py
@@ -21,7 +21,7 @@ class GumbelSoftmax(RelaxedOneHotCategorical):
         soft = super(RelaxedOneHotCategorical, self).rsample(sample_shape)
         index = soft.argmax(dim=-1, keepdim=True)
         hard = torch.zeros_like(self.logits).scatter_(-1, index, 1.)
-        return hard - soft.detach() + soft
+        return (hard - soft).detach() + soft
 
     def log_prob(self, value):
         """value is one-hot or relaxed"""

pyro/policies/adaptive_gumbel_softmax_policy.py
@@ -78,8 +78,8 @@ class AdaptiveGumbelSoftmaxPolicy(StochasticPolicy):
                  output_b_init=nn.init.zeros_,
                  init_temp=1.0,
                  learn_temp=True,
-                 min_temp=np.exp(-20.),
-                 max_temp=np.exp(2.),
+                 min_temp=0.01,
+                 max_temp=10.,
                  temp_parameterization='exp',
                  layer_normalization=False):
         super().__init__(env_spec, name='AdaptiveGumbelSoftmaxPolicy')